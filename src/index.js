d8ba10d4916fe3b8e7f617ef5134ef30ac3f7878
Phumrapee Limpianchop <git@rayriffy.com>
2019-01-08 17:17:18 +0700

    :sparkles: Fully functional for major prizes

--- src/index.js
+++ src/index.js
@@ -9,15 +9,45 @@ const REQUEST_HEADER = {
   Authorization: `Bearer ${config.line.access_token}`,
 }
 
-export let test = functions.https.onRequest((req, res) => {
-  res.status(200).send(`OK`)
-})
-
 export let linebot = functions.https.onRequest((req, res) => {
+  console.log(req.body.events[0].message)
+
   if (req.body.events[0].message.type !== 'text') {
     return
   }
   let text = req.body.events[0].message.text
 
   // TODO: GET LOTTERY NUMBER AND MAKE A PROCESS
+  axios({
+    method: 'GET',
+    url: 'https://thai-lotto-api.herokuapp.com/lotto/30122561',
+    responseType: 'json',
+  }).then(json => {
+    // Check for major prizes
+    json.data.response.prizes.forEach(prize => {
+      prize.number.forEach(number => {
+        if (number === text) {
+          axios({
+            method: 'POST',
+            url: `${LINE_API_URL}/reply`,
+            headers: REQUEST_HEADER,
+            data: {
+              replyToken: req.body.events[0].replyToken,
+              messages: [
+                {
+                  type: 'text',
+                  text: `ยินดีด้วยท่านได้รับรางวัล ${prize.name} มูลค่า ${
+                    prize.reward
+                  } บาท!!!`,
+                },
+              ],
+            },
+          }).catch(err => {
+            console.log(err.data)
+          })
+          return 0
+        }
+      })
+    })
+  })
 })
